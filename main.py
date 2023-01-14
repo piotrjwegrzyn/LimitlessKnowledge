87939094e21f839efd13f6eb6c42595605ed92ba
szymon.bobrowski <szymon.bobrowski@comarch.com>
2023-01-14 22:54:07 +0100

    Base

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-from PIL import Image
 import colour
 import numpy as np
 import matplotlib.pyplot as plot
+from PIL import Image
 
+'''
+Description
+'''
 def readImage(path):
     return Image.open(path)
 
-def imageToFreq(image):
-    rgb_im = image.convert('RGB')
-    r, g, b = rgb_im.getpixel((1, 1))
+'''
+Description
+'''
+def imageToParts(image):
+    w, h = image.size
+    
+    parts = [.2, .1, .4, .1, .2]
+    dominant_colors = list()
+    last_width = 0
+    for part_percent in parts:
+        pixels = w * part_percent
+        cropped_image = image.crop((last_width, 0, last_width + pixels, h))
+        last_width += pixels
+        dominant_colors.append(getDominantColor(cropped_image))
+    return dominant_colors
 
+'''
+Description
+'''
+def getDominantColor(image):
+    width, height = image.size
+    image = image.resize((width, height),resample = 0)
+    pixels = image.getcolors(width * height)
+    sorted_pixels = sorted(pixels, key=lambda t: t[0])
+    dominant_color = sorted_pixels[-1][1]
+    return dominant_color
+
+'''
+Description
+'''
+def RGBToWaveLength(r,g,b):
     RGB_f = np.array([r,g,b]) / 255
 
-    # Using the individual definitions:
     RGB_l = colour.models.eotf_sRGB(RGB_f)
     XYZ = colour.RGB_to_XYZ(
         RGB_l,
@@ -25,29 +54,26 @@ def imageToFreq(image):
         xy, colour.models.RGB_COLOURSPACE_sRGB.whitepoint
     )
 
-    # Using the automatic colour conversion graph:
     wl, xy_1, xy_2 = colour.convert(RGB_f, "Output-Referred RGB", "Dominant Wavelength")
 
-    print(wl, xy_1, xy_2)
     return wl, xy_1, xy_2
 
+def wavelengthToFreq(wl):
+    # freq  = speedOfLight/waveLength
+    return 299792458//wl*10e9    # This is returned in HZ
+    # return 299792458//wl    # This is returned in GHZ
 
 if __name__ == '__main__':
-    image = readImage('./assets/red.png')
-    wl, xy1, xy2 = imageToFreq(image)
+    image = readImage('./assets/green_blue.png')
 
-    # Get x values of the sine wave
+    dominant_colors = imageToParts(image)
+    wave_lengths = [RGBToWaveLength(r,g,b) for r,g,b in dominant_colors]
+    print("Dominant colors", dominant_colors)
 
-    time        = np.arange(0, 100, 10)
-    amplitude   = 1 # np.sin(time)
-
-    plot.plot(time, amplitude)
-    plot.title('Sine wave')
-    plot.xlabel('Time')
-    plot.ylabel('Amplitude = sin(time)')
-    plot.grid(True, which='both')
-    plot.axhline(y=0, color='k')
-    plot.show()
+    #Wave lengths in nano m.
+    print("Wave lengths", wave_lengths)
+    # print("Wave lengths[0]", wave_lengths[0][0])
+    print(wavelengthToFreq(wave_lengths[0][0]))
 
     # parser = argparse.ArgumentParser(
     #                 prog = 'LimitlessKnowledge',
